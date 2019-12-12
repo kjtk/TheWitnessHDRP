999893af5441285e4103c5647746881c4a5341f5
Laura L <lmlehtinen@gmail.com>
2019-12-12 13:36:16 +0200

    äänihommels

---
+++ Assets/effecttrigget.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class effecttrigget : MonoBehaviour
+
+{
+    [SerializeField]
+    private AudioSource audio;
+    public AudioClip efekti;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("player")){
+            audio.Play();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
