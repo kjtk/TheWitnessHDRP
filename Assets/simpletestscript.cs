ee82438ce823604ef67709523d18ebbe11f34dca
Laura L <lmlehtinen@gmail.com>
2019-12-15 00:43:21 +0200

    koitin tehdä valikkoskenee mut voiko olla että HDRP tekee siitäkin hankalaa? Ei-hdrp-skenessä toimi button-toiminto ihan hyvin.

---
+++ Assets/simpletestscript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundmanager : MonoBehaviour
+{
+    public AudioSource src;
+    public void PlaySound()
+    {
+
+        src.Play();
+    }
+    public void PauseSound()
+    {
+
+        src.Pause();
+    }
+    public void StopSound()
+    {
+
+        src.Stop();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
