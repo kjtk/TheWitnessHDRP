87b87059589ef9238c0290c73db15bb50b63bf6c
Laura L <lmlehtinen@gmail.com>
2019-12-18 09:57:24 +0200

    Toimiva puzzleketju alusta loppuun (kaikki puzzlet ei oo mukana)

---
+++ Assets/scenemanager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scenemanager : MonoBehaviour
+
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    [System.Obsolete]
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X)) {
+            Application.LoadLevel(Application.loadedLevel);
+
+           
+
+
+                
+            
+        }
+    } 
+
+}
+
